4c3db57f9ab16fb7b081dbdf636e3c833d28e096
Paul Fruitful <fruitful2007@outlook.com>
2022-10-30 22:42:17 +0000

    Established a relationship between User and Idea

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,12 +17,18 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
+
     protected $fillable = [
         'name',
         'email',
         'password',
+        'country',
+        'ip',
+        'followers'
     ];
-
+  public function idea(){
+    return $this->hasMany(Idea::class,'user_id');
+  }
     /**
      * The attributes that should be hidden for serialization.
      *
