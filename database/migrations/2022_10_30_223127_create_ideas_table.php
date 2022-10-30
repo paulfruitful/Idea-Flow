224f9b60686b20781372a2952cf07110a378f740
Paul Fruitful <fruitful2007@outlook.com>
2022-10-30 22:43:12 +0000

    idea migration

--- database/migrations/2022_10_30_223127_create_ideas_table.php
+++ database/migrations/2022_10_30_223127_create_ideas_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('author');
+            $table->text('email');
             $table->longText('description');
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->text('plan');
