3b9ebd4d20ee3519dd363514ac8b59dbe04cd18d
Paul Fruitful <fruitful2007@outlook.com>
2022-10-31 18:54:46 +0000

    User controller functionalities such as login, register

--- app/Http/Controllers/userControl.php
+++ app/Http/Controllers/userControl.php
@@ -2,9 +2,55 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Notifications\Welcome;
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Notification;
 
 class userControl extends Controller
 {
-    //
+      //Gets the register page
+      public function create(){
+        return view('user.register');
+    }
+    //Store users profile
+    public function store(Request $request){
+     $formData=$request->validate(
+        [
+          'name'=>['required','min:4'],
+          'email'=>['required','email',Rule::unique('users','email')],
+          'password'=>['required','min:6']
+        ]
+        );
+        $formData['password']=bcrypt($formData['password']);
+        $user=User::create($formData);
+        Notification::send($user,new Welcome($user->name));
+  
+        auth()->login($user);
+     return redirect('/')->with('success','Account Created Successfully');
+    }
+    //User logout function
+    public function logout(Request $request){
+        auth()->logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/')->with('success','Account Created Successfully');
+    }
+    // User login fuction
+    public function login(Request $request){
+        $form_data=$request->validate(
+            [
+                'email'=>['email','required'],
+                'password'=>['required','min:6']
+            ]
+            );
+            if(auth()->attempt($form_data)){
+                $request->session()->regenerate();
+                return redirect('/')->with('success','Account Logged In Successfully');
+
+            }
+            return back()->withErrors(['email'=>'Invalid Login Details'])->onlyInput('email');
+        
+    }
 }
