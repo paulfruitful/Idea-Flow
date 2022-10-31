59099cacc7bf7490769c963c465c3c7536ef8e58
Paul Fruitful <fruitful2007@outlook.com>
2022-10-31 19:09:29 +0000

    Idea_comments Migration

---
+++ database/migrations/2022_10_31_190637_create_idea_comments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('idea_comments', function (Blueprint $table) {
+            $table->id();
+            $table->string('username');
+            $table->longText('comment');
+            $table->foreignId('idea_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('idea_comments');
+    }
+};
