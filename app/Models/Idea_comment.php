df729abe460cd4ab5654b16c6d2aa6b5102a5463
Paul Fruitful <fruitful2007@outlook.com>
2022-10-31 19:17:29 +0000

    Relationship between idea model and Idea comment model

--- app/Models/Idea_comment.php
+++ app/Models/Idea_comment.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Idea;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Idea_comment extends Model
 {
     use HasFactory;
+    public function Idea(){
+        return $this->belongsTo(Idea::class,'idea_id');
+    }
 }
