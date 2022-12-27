d18fe739c44a04a26268b55d68f9b11efb782d35
Paul Fruitful <fruitful2007@outlook.com>
2022-12-27 12:36:53 +0100

    Form complete

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -142,7 +142,7 @@
                     Subscribe to our newsletter
                   </label>
                 </div>
-                <button type="button" data-mdb-ripple="true" data-mdb-ripple-color="light" class="inline-block px-6 py-2.5 mb-6 w-full bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Sign up</button>
+                <button type="submit" data-mdb-ripple="true" data-mdb-ripple-color="light" class="inline-block px-6 py-2.5 mb-6 w-full bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Sign up</button>
                 <div class="text-center">
                 
                 </div>
