e2c62ff22c95dcbeab857551751f0dcc71dce4ab
Paul Fruitful <fruitful2007@outlook.com>
2023-01-02 21:07:26 +0100

    Fixed and disabled the foreign key check for ideas table

--- database/migrations/2023_01_02_194346_uuid_idea.php
+++ database/migrations/2023_01_02_194346_uuid_idea.php
@@ -13,6 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
+        Schema::disableForeignKeyConstraints();
         Schema::table('ideas',function(Blueprint $table){
            $table->uuid('id')->change();
         });
