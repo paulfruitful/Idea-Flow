cf8feded2924ba18a6275be25efcb840beef5272
Paul Fruitful <fruitful2007@outlook.com>
2023-01-02 22:34:19 +0100

    remember me

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -95,7 +95,7 @@
                 <input type="password" name="password_confirmation" class="form-control block w-full px-3 py-1.5 mb-6 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Confirm Password"/>
                
                 <div class="form-check flex justify-center mb-6">
-                  <input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckChecked" checked>
+                  <input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="true" id="flexCheckChecked" checked>
                   <label class="form-check-label inline-block text-gray-800" for="flexCheckChecked">
                     Keep me logged in
                   </label>
