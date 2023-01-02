dfaa90029ea00cccf9f2c43739b6879a3740604b
Paul Fruitful <fruitful2007@outlook.com>
2023-01-02 21:11:13 +0100

    Migration for users with UUIds

--- database/migrations/2023_01_02_200931_uuid_for_users.php
+++ database/migrations/2023_01_02_200931_uuid_for_users.php
@@ -13,7 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        //
+        Schema::table('users',function(Blueprint $table){
+         
+        });
     }
 
     /**
