1ff00c371eb202550566026151c82941b0aa889e
Paul Fruitful <fruitful2007@outlook.com>
2023-01-02 23:21:49 +0100

    Landing page edits

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -101,7 +101,7 @@
                   </label>
                 </div>
                 <button type="submit" data-mdb-ripple="true" data-mdb-ripple-color="light" class="inline-block px-6 py-2.5 mb-6 w-full bg-indigo-700 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-indigo-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-indigo-800 active:shadow-lg transition duration-150 ease-in-out">Sign up</button>
-                <div class="text-center">
+                <div class="text-center text-sm">
                 Already Registered? <span class="text-indigo-800">Login</span>
                 </div>
               <div class="flex justify-center">
