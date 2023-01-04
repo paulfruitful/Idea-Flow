aaef69954a64b4d9c3836617daf424558cfb24cb
Paul Fruitful <fruitful2007@outlook.com>
2023-01-04 14:55:15 +0100

    Navbar component

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,8 +1,8 @@
 <div class="flex p-6 rounded-sm shadow-xl bg-white ">
 <div class="flex flex-row">
-  <a href="" class="text-2xl text-gray-800">Idea Pools</a>
-  <a href="">Solution Pools</a>
-  <a href="">Problem Pools</a>
+  <a href="" class="text-xl font-bold p-6 text-gray-800">Idea Pools</a>
+  <a href="" class="text-xl font-bold p-6 text-gray-800">Solution Pools</a>
+  <a href="" class="text-xl font-bold p-6 text-gray-800">Problem Pools</a>
 </div>
 
 </div>
