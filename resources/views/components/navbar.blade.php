4956023f885f6c2e70108a830fc8fd444bbccfa1
Paul Fruitful <fruitful2007@outlook.com>
2023-01-04 14:46:49 +0100

    Navbar component

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,4 +1,8 @@
 <div class="flex p-6 rounded-sm shadow-xl bg-white ">
-
+<div class="flex flex-row-reverse">
+  <a href="">Idea Pools</a>
+  <a href="">Solution Pools</a>
+  <a href="">Problem Pools</a>
+</div>
 
 </div>
