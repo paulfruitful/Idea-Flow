d722fc836ea4b3100806ad712a22c1922897cf31
Paul Fruitful <fruitful2007@outlook.com>
2023-01-04 20:17:17 +0100

    Navbar component

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,10 +1,10 @@
-<div class="grid p-4 rounded-sm justify-items-stretch shadow-xl bg-white ">
+<div class="grid p-4 rounded-sm justify-items-stretch shadow-md bg-white ">
 
 <div class="relative justify-self-end">
 <div class="flex flex-row  text-center">
-  <a href="" class="text-md  font-bold p-3 shadow-sm text-dark">Idea Pools</a>
-  <a href="" class="text-md  font-bold p-3 shadow-sm text-dark">Solution Pools</a>
-  <a href="" class="text-md font-bold p-3 shadow-sm text-dark">Problem Pools</a>
+  <a href="" class="text-md  font-bold p-3 shadow-sm text-indigo-900">Idea Pools</a>
+  <a href="" class="text-md  font-bold p-3 shadow-sm text-indigo-900">Solution Pools</a>
+  <a href="" class="text-md font-bold p-3 shadow-sm text-indigo-900">Problem Pools</a>
 </div>
 </div>
 </div>
