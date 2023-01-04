345d02f06eac1dd174e3930b4699d9aee332d43c
Paul Fruitful <fruitful2007@outlook.com>
2023-01-04 15:17:51 +0100

    Navbar component

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,7 +1,7 @@
 <div class="flex p-6 rounded-sm shadow-xl bg-white ">
-<div class="flex flex-row relative right-0">
-  <a href="" class="text-md font-bold p-3 shadow-sm text-indigo-800">Idea Pools</a>
-  <a href="" class="text-md font-bold p-3 shadow-sm text-indigo-800">Solution Pools</a>
+<div class="flex flex-row relative absolute">
+  <a href="" class="text-md  font-bold p-3 shadow-sm text-indigo-800">Idea Pools</a>
+  <a href="" class="text-md  font-bold p-3 shadow-sm text-indigo-800">Solution Pools</a>
   <a href="" class="text-md font-bold p-3 shadow-sm text-indigo-800">Problem Pools</a>
 </div>
 
