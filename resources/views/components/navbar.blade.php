8914edc79f5b8d0a5e486a042ef4ff28ef0f3d81
Paul Fruitful <fruitful2007@outlook.com>
2023-01-05 16:37:52 +0100

    Nav brand

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -7,7 +7,9 @@
 </style>
 
 <div class="grid p-4 rounded-sm justify-items-stretch shadow-sm bg-white ">
-
+<div class="justify-self-start">
+  <img src="https://i.ibb.co/rkmPTVx/Idea.png" alt="">
+</div>
 <div class="relative justify-self-end">
 <div class="flex flex-row sm:flex-column  text-center">
   <a href="" class="text-md  font-bold p-3 hover:p-4 flex flex-nowrap flex-row hover:text-indigo-500 text-gray-800">
