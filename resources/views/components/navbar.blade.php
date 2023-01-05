598b6e5083ae2a3f115f68d196cc1e25b5522b6c
Paul Fruitful <fruitful2007@outlook.com>
2023-01-05 15:41:22 +0100

    Still on the navbar component

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -2,9 +2,9 @@
 
 <div class="relative justify-self-end">
 <div class="flex flex-row sm:flex-column  text-center">
-  <a href="" class="text-md  font-bold p-3 shadow-sm text-indigo-900">Idea Pools</a>
-  <a href="" class="text-md  font-bold p-3 shadow-sm text-indigo-900">Solution Pools</a>
-  <a href="" class="text-md font-bold p-3 shadow-sm text-indigo-900">Problem Pools</a>
+  <a href="" class="text-md  font-bold p-3 hover:p-4 hover:text-indigo-600 text-indigo-900">Idea Pools</a>
+  <a href="" class="text-md  font-bold p-3 hover:p-4 hover:text-indigo-600 text-indigo-900">Solution Pools</a>
+  <a href="" class="text-md font-bold p-3 hover:p-4 hover:text-indigo-600 text-indigo-900">Problem Pools</a>
 </div>
 </div>
 </div>
