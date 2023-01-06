1e7a0e17fe43d172ddb823e92b473f4622628dad
Paul Fruitful <fruitful2007@outlook.com>
2023-01-06 23:24:41 +0100

    Hero component

--- resources/views/components/pool-hero.blade.php
+++ resources/views/components/pool-hero.blade.php
@@ -1,4 +1,4 @@
-<div class="grid justify-self-stretch justify-items-center bg-gradient-to-r from-indigo-800 to-sky-500  w-full" style="height: 200px">
+<div class="grid justify-self-stretch justify-items-center bg-gradient-to-r from-indigo-900 to-sky-300  w-full" style="height: 450px">
 <div class="relative justify-self-end flex flex-row">
 
 </div>
