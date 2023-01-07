5e6768726b8625f6b7557c7f324e619daee79cf1
Paul Fruitful <fruitful2007@outlook.com>
2023-01-07 23:29:54 +0100

    Hero component

--- resources/views/components/pool-hero.blade.php
+++ resources/views/components/pool-hero.blade.php
@@ -3,7 +3,9 @@
 <div class="m-auto flex flex-col">
 <p class="text-5xl text-white m-auto font-bold "><span class="">Let Your</span>  Ideas Flow </p>
 <p class="text-xl font-bold p-6 mb-4" style="color: #ffffff">Share Your Ideas, Offer Your Solutions and Report Your Problems </p>
-<a href="/ideas/create" class="p-6 text-md flex flex-row text-white font-bold border-solid shadow-xl rounded-md" style="bg-color:#ffea00;">Share Ideas</a>
+<div class="justify-items-center m-auto">
+<a href="/ideas/create" class="p-4 m-auto text-md text-gray-800 font-bold border-solid shadow-xl rounded-md" style="background-color:#ffea00;">Share Ideas</a>
+
 </div>
 </div>
 </div>
