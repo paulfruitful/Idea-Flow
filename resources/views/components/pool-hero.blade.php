6590de304eb894052605a9966d12756d1ab69f8f
Paul Fruitful <fruitful2007@outlook.com>
2023-01-07 23:00:12 +0100

    Hero component

--- resources/views/components/pool-hero.blade.php
+++ resources/views/components/pool-hero.blade.php
@@ -1,7 +1,7 @@
 <div class="grid justify-self-stretch justify-items-center mt-3  bg-gradient-to-r from-cyan-500 to-blue-700 w-full" style="height: 450px">
 <div class=" justify-self-center flex flex-row">
 <div class="m-auto">
-<p class="text-5xl text-yellow-200 font-bold"><span class="">Let Your</span>  Ideas Flow </p>
+<p class="text-5xl text-white font-bold"><span class="">Let Your</span>  Ideas Flow </p>
 </div>
 </div>
 </div>
