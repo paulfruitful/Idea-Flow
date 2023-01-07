05f98f63b1d5f94c38a09bd83ee515a166fe0cd7
Paul Fruitful <fruitful2007@outlook.com>
2023-01-07 03:16:00 +0100

    Hero component

--- resources/views/components/pool-hero.blade.php
+++ resources/views/components/pool-hero.blade.php
@@ -1,5 +1,7 @@
 <div class="grid justify-self-stretch justify-items-center mt-3  bg-gradient-to-r from-cyan-500 to-blue-700 w-full" style="height: 450px">
-<div class="relative justify-self-end flex flex-row">
-
+<div class="relative justify-self-center flex flex-row">
+<div class="text-center">
+Let Your Ideas Flow
+</div>
 </div>
 </div>
