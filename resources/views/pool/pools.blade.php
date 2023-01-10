c8d71b87db422c3c6dead9b4af340bdecec52632
Paul Fruitful <fruitful2007@outlook.com>
2023-01-10 12:07:28 +0100

    New sections

--- resources/views/pool/pools.blade.php
+++ resources/views/pool/pools.blade.php
@@ -6,8 +6,9 @@
   <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
+  <div class="">
     <x-navbar/>
     <x-pool-hero/>
-    <h1 class="text-center font-bold p-6 text-black">Latest Ideas!</h1>
+  </div>
 </body>
 </html>
