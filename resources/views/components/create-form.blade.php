8f1b1af98bcb104bc47d9b8a37080d52311837c9
Paul Fruitful <fruitful2007@outlook.com>
2023-01-12 21:40:11 +0100

    Create an Idea : Form border revert

--- resources/views/components/create-form.blade.php
+++ resources/views/components/create-form.blade.php
@@ -2,6 +2,6 @@
     <div class="relative flex flex-row m-6 p-6 text-center"><h1 class="font-bold text-3xl">Share an Idea</h1></div>
     <div class="relative flex flex-row m-6">
     <label  class="text-xl pr-3">Title:</label>
-    <input type="text" placeholder="Idea to solve..." class=" placeholder:italic placeholder:text-slate-400 block border-solid border-t-0 border-b-2 border-gray-600 focus:outline-none focus:border-sky-500" name="title">
+    <input type="text" placeholder="Idea to solve..." class=" placeholder:italic placeholder:text-slate-400 block border-solid border-t-0 border-b-2 border-slate-300  focus:outline-none focus:border-sky-500" name="title">
 </div>
 </form>
