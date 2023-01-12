dc73fa6d21e897db5dccec3a7b505aeb40a4d3e8
Paul Fruitful <fruitful2007@outlook.com>
2023-01-12 01:37:13 +0100

    The create Idea functionality

--- resources/views/components/create-form.blade.php
+++ resources/views/components/create-form.blade.php
@@ -1,3 +1,6 @@
 <form action="/create/idea" class="flex flex-column p-6 bg-white">
-
+<div class="flex flex-column">
+    <label  class="font-bold p-6 text-xl shadow-sm">Title</label>
+    <input type="text" class="p-6" name="title">
+</div>
 </form>
