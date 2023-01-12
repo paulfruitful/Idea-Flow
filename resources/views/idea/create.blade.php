340cab89e71c696795ea5642fcdb7255a9a1b293
Paul Fruitful <fruitful2007@outlook.com>
2023-01-12 20:55:32 +0100

    Create an Idea

--- resources/views/idea/create.blade.php
+++ resources/views/idea/create.blade.php
@@ -1,3 +1,15 @@
-<div class="grid justify-items-center">
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdn.tailwindcss.com"></script>
+</head>
+ <body>
+   
+ <x-navbar/>
+ <div class="grid justify-items-center">
+   
     <x-create-form/>
-</div>
+</div> 
+ </body>
