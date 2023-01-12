9aa206003f2f262daf7733d975392476137e2567
Paul Fruitful <fruitful2007@outlook.com>
2023-01-12 21:38:45 +0100

    Create an Idea : Form background revert

--- resources/views/components/create-form.blade.php
+++ resources/views/components/create-form.blade.php
@@ -1,7 +1,7 @@
-<form action="/create/idea" class="block bg-gradient-to-r from-cyan-500 to-blue-700 p-12 m-6 border-solid border-2  shadow-xl  rounded-md bg-white">
-    <div class="relative flex flex-row m-6 p-6 text-center"><h1 class="font-bold text-white text-3xl">Share an Idea</h1></div>
+<form action="/create/idea" class="block  p-12 m-6 border-solid border-2  shadow-xl  rounded-md bg-white">
+    <div class="relative flex flex-row m-6 p-6 text-center"><h1 class="font-bold text-3xl">Share an Idea</h1></div>
     <div class="relative flex flex-row m-6">
-    <label  class="text-xl text-white pr-3">Title:</label>
-    <input type="text" placeholder="Idea to solve..." class=" placeholder:italic placeholder:text-slate-400 placeholder:text-white block bg-transparent border-solid border-t-0 border-b-2 border-white focus:outline-none focus:border-yellow-300 w-59" name="title">
+    <label  class="text-xl pr-3">Title:</label>
+    <input type="text" placeholder="Idea to solve..." class=" placeholder:italic placeholder:text-slate-400 block border-solid border-t-0 border-b-2 border-gray-600 focus:outline-none focus:border-sky-500" name="title">
 </div>
 </form>
