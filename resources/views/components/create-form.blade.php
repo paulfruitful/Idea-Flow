94f4ab251a5661ff702cbfd4f1810fc5273d2ee4
Paul Fruitful <fruitful2007@outlook.com>
2023-01-12 22:36:56 +0100

    😀

--- resources/views/components/create-form.blade.php
+++ resources/views/components/create-form.blade.php
@@ -3,7 +3,7 @@
     <div class="relative flex flex-row m-6 p-6 text-center"><h1 class="font-bold text-3xl">Share an Idea</h1></div>
     <div class="relative flex flex-row m-6">
     <label  class="text-xl pr-3">Title:</label>
-    <input type="text" placeholder="Idea to solve..." class=" placeholder:italic placeholder:text-slate-400 block border-solid border-t-0 border-b-2 border-slate-600  focus:outline-none focus:border-sky-500" style="width: 240px" name="title">
+    <input type="text" placeholder="Idea to solve..." class=" placeholder:italic placeholder:text-slate-400 block border-solid border-t-0 border-b-2 border-slate-600  focus:outline-none focus:border-sky-500" style="width: 240px; border-bottom-width:1px;" name="title">
 </div>
 <div class="relative flex flex-col m-6 mt-6">
     <input type="text" placeholder="Idea to solve..." id="edit" class="  placeholder:italic placeholder:text-slate-400 block border-solid border-2 p-6 border-slate-400  focus:outline-none focus:border-sky-500" name="description">
