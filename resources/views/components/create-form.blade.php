e8d8cef22d8ef94f7a55b3cc2b1b30db04cfe858
Paul Fruitful <fruitful2007@outlook.com>
2023-01-13 20:29:17 +0100

    Create an Idea : monetization! 😀

--- resources/views/components/create-form.blade.php
+++ resources/views/components/create-form.blade.php
@@ -1,5 +1,6 @@
 <script src="//cdn.ckeditor.com/4.20.1/basic/ckeditor.js"></script>
 <form action="/create/idea" class="block  p-12 m-6 border-solid border-2  shadow-xl  rounded-md bg-white">
+    @csrf
     <div class="relative flex flex-row m-6 p-6 text-center"><h1 class="font-bold text-3xl">Share an Idea</h1></div>
     <div class="relative flex flex-row m-6">
     <label  class="text-xl pr-3">Title:</label>
@@ -8,6 +9,12 @@
 <div class="relative flex flex-col m-6 mt-6">
     <input type="text" placeholder="Idea to solve..." id="edit" class="  placeholder:italic placeholder:text-slate-400 block border-solid border-2 p-6 border-slate-400  focus:outline-none focus:border-sky-500" name="description">
 <span class="flex flex-row"><span class="text-gray-400 italic text-md text-center pr-2">Write a brief description about your idea </span>😍</span></div>
+<div class="relative flex flex-row m-6">
+<select class="focus:bg-white focus:text-white p-3" aria-placeholder="Monetize" name="spons" id="">
+    <option value="true">Yes</option>
+    <option value="false">No</option>
+</select>
+</div>
 </form>
 
 <script>
