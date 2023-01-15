e2af6b22659542f7d7985973c06c093aaf025c0d
Paul Fruitful <fruitful2007@outlook.com>
2023-01-15 16:47:30 +0100

    🤔

--- resources/views/pool/pools.blade.php
+++ resources/views/pool/pools.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="shortcut icon" href="fan.svg" type="image/svg">
+  <link rel="icon" type="image/x-icon" href="fan-solid.svg">
   <title>IdeaFlow</title>
   <script src="https://cdn.tailwindcss.com"></script>
 </head>
