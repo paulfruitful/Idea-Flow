40920410a1897ab8ad0063af748c17861167b2f5
Paul Fruitful <fruitful2007@outlook.com>
2023-01-16 20:40:23 +0100

    Minor change

--- resources/views/pool/pools.blade.php
+++ resources/views/pool/pools.blade.php
@@ -7,7 +7,7 @@
   <title>IdeaFlow</title>
   <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
+<body class="block">
   <div class="flex-column">
     <x-navbar/>
     <x-pool-hero/>
