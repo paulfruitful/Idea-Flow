f2ddb1f29a1bea8ba91b29f7ce84655275c75fcb
Paul Fruitful <fruitful2007@outlook.com>
2023-01-18 23:25:42 +0100

    The Idea component

--- resources/views/components/idea.blade.php
+++ resources/views/components/idea.blade.php
@@ -1,6 +1,7 @@
 @props(['idea'])
-
-<div class="block p-6 relative bg-white text-black shadow-md m-4">
+<a href="/ideas/{{$idea->id}}" class="">
+<div class="block p-6 relative bg-white hover:shadow-lg hover:p-8 text-black shadow-md m-4">
 <div class="text-center font-bold p-6 m-3 text-xl">{{$idea->title}}</div>
-<div class="text-md">By {{ucwords($idea->author)}}</div>
+<div class="text-md ">By <a href="/ideas/{{$idea->id}}" class=" font-bold">{{ucwords($idea->author)}}</a></div>
 </div>
+</a>
