f6ed828342a1a1f949d1cf16bdbb4027abbd1219
Paul Fruitful <fruitful2007@outlook.com>
2023-01-18 23:17:36 +0100

    The Idea component

--- resources/views/components/idea.blade.php
+++ resources/views/components/idea.blade.php
@@ -1,6 +1,6 @@
 @props(['idea'])
 
 <div class="block p-6 relative bg-white text-black shadow-md m-4">
-<div class="text-center">{{$idea->title}}</div>
-
+<div class="text-center font-bold p-6 m-3 text-xl">{{$idea->title}}</div>
+<div class="text-md">By {{ucwords($idea->author)}}</div>
 </div>
