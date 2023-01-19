59db4bce49956ef328c191e95b9b289a5b73a1ba
Paul Fruitful <fruitful2007@outlook.com>
2023-01-19 23:09:05 +0100

    Taglines

--- resources/views/components/idea.blade.php
+++ resources/views/components/idea.blade.php
@@ -1,8 +1,9 @@
 @props(['idea'])
 <a href="/ideas/{{$idea->id}}" class="">
 <div class="block p-6 relative bg-white hover:shadow-lg hover:p-8 text-black shadow-md m-4">
+    <div class="text-center flex flex-row flex-row-reverse p-6 text-blue-600 text-md"><img src="https://cdn-icons-png.flaticon.com/512/9135/9135999.png" class="" style="height: 30px; width:30px; margin-top:-5px;" alt=""><p class="mr-2">{{$idea->tagline}} </p></div>
+
 <div class="text-center font-bold p-6 text-xl">{{$idea->title}}</div>
-<div class="text-center flex flex-row flex-row-reverse p-6 text-blue-600 text-md"><img src="https://cdn-icons-png.flaticon.com/512/9135/9135999.png" style="height: 30px; width:30px;" alt="">{{$idea->tagline}} </div>
 <div class="relative grid">
 <div class="relative text-md mb-3 justify-self-start">By <a href="/ideas/{{$idea->id}}" class=" font-bold">{{ucwords($idea->author)}}</a></div>
 <div class=" relative justify-self-end ">
