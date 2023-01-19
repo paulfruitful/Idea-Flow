b700cab25b5be357b69584b10b9c015af6c75cdc
Paul Fruitful <fruitful2007@outlook.com>
2023-01-19 22:50:48 +0100

    Taglines

--- resources/views/components/idea.blade.php
+++ resources/views/components/idea.blade.php
@@ -1,8 +1,8 @@
 @props(['idea'])
 <a href="/ideas/{{$idea->id}}" class="">
 <div class="block p-6 relative bg-white hover:shadow-lg hover:p-8 text-black shadow-md m-4">
-<div class="text-center font-bold p-63text-xl">{{$idea->title}}</div>
-<div class="text-center p-6 text-blue-600 text-md">"{{$idea->tagline}}"</div>
+<div class="text-center font-bold p-6 text-xl">{{$idea->title}}</div>
+<div class="text-center p-6 text-blue-600 text-md"><span class="text-black font-bold">"</span> {{$idea->tagline}} <span class="text-black font-bold">"</span> </div>
 <div class="relative grid">
 <div class="relative text-md mb-3 justify-self-start">By <a href="/ideas/{{$idea->id}}" class=" font-bold">{{ucwords($idea->author)}}</a></div>
 <div class=" relative justify-self-end ">
