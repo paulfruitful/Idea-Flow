8396e7a36dbea3985945c6595cef8ae49667cd99
Paul Fruitful <fruitful2007@outlook.com>
2023-01-22 23:20:22 +0100

    Idea DEscription component

--- resources/views/components/description.blade.php
+++ resources/views/components/description.blade.php
@@ -1,6 +1,6 @@
 @props(['idea'])
 <div class="flex mt-12 flex-col w-full p-6">
-    <div class="text-center mb-5 font-bold text-3xl">Description</div>
+    <div class="text-center mb-3 font-bold text-3xl">Description</div>
 <div class="p-6 text-xl">
     <p>{{$idea->description}}</p>
 </div>
