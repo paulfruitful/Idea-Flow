d160ff8a745176788bf32abb3d7a9e8c2beb2738
Paul Fruitful <fruitful2007@outlook.com>
2023-01-22 23:22:32 +0100

    Idea DEscription component

--- resources/views/components/description.blade.php
+++ resources/views/components/description.blade.php
@@ -1,8 +1,12 @@
 @props(['idea'])
 <div class="flex mt-12 flex-col w-full p-6">
     <div class="text-center mb-3 font-bold text-3xl">Description</div>
-<div class="p-6 text-xl">
+ <div class="grid justify-self-stretch">
+<div class="justify-self-end"><p class="text-md">By <span class="font-bold">{{$idea->author}}</span></p></div>
+ </div>
+    <div class="p-6 text-xl">
     <p>{{$idea->description}}</p>
+
 </div>
 
 </div>
