17aa493ecf37cb7783a806acea4a0836c86b1077
Paul Fruitful <fruitful2007@outlook.com>
2023-01-22 23:27:23 +0100

    Idea DEscription component

--- resources/views/components/description.blade.php
+++ resources/views/components/description.blade.php
@@ -3,7 +3,8 @@
     <div class="text-center mb-3 font-bold text-3xl">Description</div>
  <div class="grid justify-self-stretch">
 <div class="justify-self-end"><p class="text-md">By <span class="font-bold">{{ucwords($idea->author)}}</span></p></div>
- </div>
+<div class="justify-self-start"><span class="p-6 shadow-sm bg-gradient-to-r from-cyan-500 to-blue-700 border-solid rounded">{{$idea->plan=="combined"?"Software X Hardware":$idea->plan}}</span></div> 
+</div>
     <div class="p-6 text-xl">
     <p>{{$idea->description}}</p>
 
