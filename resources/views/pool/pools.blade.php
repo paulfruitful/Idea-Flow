64e28269b2f271cb971be423a66957c9fa3bebd1
Paul Fruitful <fruitful2007@outlook.com>
2023-01-22 22:17:01 +0100

    See more

--- resources/views/pool/pools.blade.php
+++ resources/views/pool/pools.blade.php
@@ -16,5 +16,9 @@
   <div>
     <x-latest-ideas :ideas="$ideas"/>
   </div>
+  <div class="text-center text-3xl font-bold text-black">
+    <a class="flex flex-row hover:text-blue-600 hover:px-3 " href="">See more <img src="https://cdn-icons-png.flaticon.com/512/2989/2989988.png" style="height: 30px; width:30px;" alt=""> </a>
+
+ </div>
 </body>
 </html>
