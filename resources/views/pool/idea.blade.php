400dbe5b323195bebbab298ad7080369c567b9fc
Paul Fruitful <fruitful2007@outlook.com>
2023-01-22 23:32:22 +0100

    Idea Pool

---
+++ resources/views/pool/idea.blade.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" type="image/x-icon" href="fan-solid.svg">
+  <title>IdeaFlow</title>
+  <script src="https://cdn.tailwindcss.com"></script>
+</head>
