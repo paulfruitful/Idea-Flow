4d51bc6d0f1036ae6539a27cb93f42749673d651
Paul Fruitful <fruitful2007@outlook.com>
2023-01-24 04:50:35 +0100

    The footer component

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -2,7 +2,12 @@
 <div class="relative grid justify-self-stretch">
     <div class="relative justify-self-start">
         <div class="flex flex-col">
-            
+            <div class="font-bold text-xl text-white p-3"><p>Pools</p></div>
+            <ul class="p-3">
+                <li class="text-md font-bold">Idea Pool</li>
+                <li class="text-md font-bold">Problem Pool</li>
+                <li class="text-md font-bold">Solution Pool</li>            
+            </ul>
         </div>
     </div>
 </div>
