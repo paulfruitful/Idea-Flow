4d337a6c5f9022ddc09d1b5414c257ae972a437f
Paul Fruitful <fruitful2007@outlook.com>
2023-01-24 04:19:23 +0100

    The footer component

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,9 @@
 <div class="bg-black-p-6 w-full rounded-t-md rounded-b-md shadow-sm text-white h-full block">
 <div class="relative grid justify-self-stretch">
-    
+    <div class="relative justify-self-start">
+        <div class="flex flex-col">
+            
+        </div>
+    </div>
 </div>
 </div>
