85eebdd48805628896d941361bb83931a059e3ff
Paul Fruitful <fruitful2007@outlook.com>
2023-01-24 05:23:56 +0100

    The footer component

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,7 @@
 <div class="bg-black p-6 w-full  shadow-sm text-white h-full block">
 <div class="relative grid justify-self-stretch">
     <div class="relative flex flex-row">
-    <div class="relative justify-self-start">
+    <div class="relative justify-self-start mr-6">
         <div class="flex flex-col">
             <div class="font-bold text-2xl text-white p-3"><p>Pools</p></div>
             <ul class="p-2">
@@ -12,7 +12,7 @@
         </div>
     </div>
 
-    <div class="relative justify-self-center">
+    <div class="justify-self-center">
         <div class="flex flex-col">
             <div class="font-bold text-2xl text-white p-3"><p>Pools</p></div>
             <ul class="p-2">
