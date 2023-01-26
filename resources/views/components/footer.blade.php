b5243c7dfe4dc27f05206e1859be836627d3e36d
Paul Fruitful <fruitful2007@outlook.com>
2023-01-26 21:41:37 +0100

    Tweaks on footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,27 +1,5 @@
 <div class="bg-black p-6 w-full  shadow-sm text-white h-full block">
-<div class="relative grid justify-self-stretch">
-    <div class="relative flex flex-row">
-    <div class="relative justify-self-start mr-6">
-        <div class="flex flex-col">
-            <div class="font-bold text-2xl text-white p-3"><p>Pools</p></div>
-            <ul class="p-2">
-                <li class="text-md"><a href=""> Idea Pool</a></li>
-                <li class="text-md"><a href=""> Problem Pool</a></li>
-                <li class="text-md"><a href=""> Solution Pool</a></li>            
-            </ul>
-        </div>
-    </div>
-
-    <div class="justify-self-center">
-        <div class="flex flex-col">
-            <div class="font-bold text-2xl text-white p-3"><p>Pools</p></div>
-            <ul class="p-2">
-                <li class="text-md"><a href=""> Idea Pool</a></li>
-                <li class="text-md"><a href=""> Problem Pool</a></li>
-                <li class="text-md"><a href=""> Solution Pool</a></li>            
-            </ul>
-        </div>
-    </div>
-</div>
+<div class="relative flex flex-row">
+   
 </div>
 </div>
