86316172e4b025455fecea9032a574035f90f8be
Paul Fruitful <fruitful2007@outlook.com>
2023-01-26 21:43:48 +0100

    Tweaks on footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,5 @@
-<div class="bg-black p-6 w-full  shadow-sm text-white h-full block">
-<div class="relative flex flex-row">
-   
+<div class="bg-black w-full  shadow-sm text-white">
+<div class="relative grid  justify-items-center">
+   <p class="text-sm font-bold">@IdeaFlow 2023 </p>
 </div>
 </div>
