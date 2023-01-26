b045a9819859864979b89832a04b13a1a6e33b59
Paul Fruitful <fruitful2007@outlook.com>
2023-01-26 21:53:25 +0100

    Tweaks, Tweaks, Tweaks...

--- resources/views/components/description.blade.php
+++ resources/views/components/description.blade.php
@@ -2,10 +2,10 @@
 <div class="flex mt-12 flex-col w-full p-6">
     <div class="text-center mb-3 font-bold text-3xl">Description</div>
  <div class="grid justify-self-stretch">
-<div class="justify-self-end"><p class="text-md">By <span class="font-bold">{{ucwords($idea->author)}}</span></p></div>
-<div class="justify-self-start"><span class="p-3 shadow-sm bg-black mb-12 text-white border-solid rounded">{{$idea->plan=="combined"?"Software X Hardware":ucwords($idea->plan)}}</span></div> 
+<div class="justify-self-end"><p class="text-md ">By <span class="font-bold">{{ucwords($idea->author)}}</span></p></div>
+<div class="justify-self-start"><span class="p-3 shadow-sm bg-black text-white border-solid rounded">{{$idea->plan=="combined"?"A Software X Hardware":'A' .' '. ucwords($idea->plan)}}</span></div> 
 </div>
-    <div class="p-6 text-xl">
+    <div class="p-6 text-xl mt-4">
     <p>{{$idea->description}}</p>
 
 </div>
--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,4 +1,4 @@
-<div class="bg-black w-full  shadow-sm text-white">
+<div class="bg-black p-3 w-full  shadow-sm text-white">
 <div class="relative grid  justify-items-center">
    <p class="text-sm font-bold">@IdeaFlow 2023 </p>
 </div>
