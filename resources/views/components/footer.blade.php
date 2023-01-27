e93548cc80f6f54f626e583fc58efbb7d1c297d6
Paul Fruitful <fruitful2007@outlook.com>
2023-01-27 21:38:20 +0100

    Tweaks for Mobile view

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,5 @@
-<div class="bg-black p-3 w-full  shadow-sm text-white">
+<div class="bg-white p-3 w-full  shadow-md border-solid border-2 text-gray-600">
 <div class="relative grid  justify-items-center">
-   <p class="text-sm font-bold">@IdeaFlow 2023 </p>
+   <p class="text-sm font-bold"><a href="/pools"></a>@IdeaFlow 2023 </p>
 </div>
 </div>
