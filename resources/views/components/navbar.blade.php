f2c334254b6a1d19cf283719e311d9bede83dbc1
Paul Fruitful <fruitful2007@outlook.com>
2023-01-30 23:05:00 +0100

    Tweaks for Mobile view: Nav

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -7,8 +7,7 @@
 </style>
 
 <script>
-function nav(e) {
-  e.preventDefault()
+function nav() {
   var x = document.getElementById("navbar");
   if (x.style.display =="flex") {
     x.style.display = "none";
@@ -36,13 +35,13 @@ function nav(e) {
   <a href="/user/{{auth()->user()->username}}" class="text-md  font-bold p-3 hover:p-4 flex flex-nowrap flex-row hover:text-indigo-500 text-gray-800 ">
     <svg xmlns="http://www.w3.org/2000/svg" class="svg mr-3" viewBox="0 0 448 512"><!-- Font Awesome Pro 5.15.4 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) --><path d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z"/></svg>
   </a></div>
- <button class="text-md font-bold p-3 hover:p-4 grid justify-self-end lg:hidden flex-row " onclick="nav(e)" id="icon">
+ <button class="text-md font-bold p-3 hover:p-4 grid justify-self-end lg:hidden flex-row " onclick="nav()" id="icon">
   <svg xmlns="http://www.w3.org/2000/svg" class="svg" viewBox="0 0 512 512"><!-- Font Awesome Pro 5.15.4 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) --><path d="M149.333 216v80c0 13.255-10.745 24-24 24H24c-13.255 0-24-10.745-24-24v-80c0-13.255 10.745-24 24-24h101.333c13.255 0 24 10.745 24 24zM0 376v80c0 13.255 10.745 24 24 24h101.333c13.255 0 24-10.745 24-24v-80c0-13.255-10.745-24-24-24H24c-13.255 0-24 10.745-24 24zM125.333 32H24C10.745 32 0 42.745 0 56v80c0 13.255 10.745 24 24 24h101.333c13.255 0 24-10.745 24-24V56c0-13.255-10.745-24-24-24zm80 448H488c13.255 0 24-10.745 24-24v-80c0-13.255-10.745-24-24-24H205.333c-13.255 0-24 10.745-24 24v80c0 13.255 10.745 24 24 24zm-24-424v80c0 13.255 10.745 24 24 24H488c13.255 0 24-10.745 24-24V56c0-13.255-10.745-24-24-24H205.333c-13.255 0-24 10.745-24 24zm24 264H488c13.255 0 24-10.745 24-24v-80c0-13.255-10.745-24-24-24H205.333c-13.255 0-24 10.745-24 24v80c0 13.255 10.745 24 24 24z"/></svg>
  </button>
 
 </div>
 <!-- Mobile View -->
-<div class=" bg-white h-full p-6 flex flex-col lg:hidden text-center" id="navbar" style="margin-top: -58px; display:none;">
+<div class=" bg-white h-full p-6  flex flex-col lg:hidden text-center" id="navbar" style="margin-top: -58px; display:none;">
   
   <a href="/pools/ideas" class="text-md  font-bold p-3 hover:p-4 flex flex-nowrap flex-row hover:text-indigo-500 text-gray-800 ">
     <svg xmlns="http://www.w3.org/2000/svg" class="svg mr-4 "viewBox="0 0 512 512"><!-- Font Awesome Pro 5.15.4 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) --><path d="M502.63 214.63l-45.25-45.25c-6-6-14.14-9.37-22.63-9.37H384V80c0-26.51-21.49-48-48-48H176c-26.51 0-48 21.49-48 48v80H77.25c-8.49 0-16.62 3.37-22.63 9.37L9.37 214.63c-6 6-9.37 14.14-9.37 22.63V320h128v-16c0-8.84 7.16-16 16-16h32c8.84 0 16 7.16 16 16v16h128v-16c0-8.84 7.16-16 16-16h32c8.84 0 16 7.16 16 16v16h128v-82.75c0-8.48-3.37-16.62-9.37-22.62zM320 160H192V96h128v64zm64 208c0 8.84-7.16 16-16 16h-32c-8.84 0-16-7.16-16-16v-16H192v16c0 8.84-7.16 16-16 16h-32c-8.84 0-16-7.16-16-16v-16H0v96c0 17.67 14.33 32 32 32h448c17.67 0 32-14.33 32-32v-96H384v16z"/></svg>
