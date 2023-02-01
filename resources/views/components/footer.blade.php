3ab57029eace7d485005313c1c84e9f182bb0e49
Paul Fruitful <fruitful2007@outlook.com>
2023-02-01 19:46:20 +0100

    Footer width tweak

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,4 +1,4 @@
-<div class="bg-white p-3 w-full  shadow-md border-solid border-2 text-gray-600">
+<div class="bg-white p-3 w-full  shadow-md border-solid border-2 text-gray-600" style="width: 100vw;">
 <div class="relative grid  justify-items-center">
    <p class="text-sm font-bold"><a href="/pools"></a>@IdeaFlow 2023 </p>
 </div>
