7c003cfc06d2a4e19bc8ed6934a67fb6209a43a6
Paul Fruitful <fruitful2007@outlook.com>
2023-02-01 19:45:22 +0100

    Comment form width tweak

--- resources/views/components/comment.blade.php
+++ resources/views/components/comment.blade.php
@@ -2,7 +2,7 @@
     <div class="text-center font-bold text-2xl p-6"><p>Drop A Comment</p></div>
     <div>
     <div class="grid justify-self-center p-6 w-full">
-    <textarea placeholder="What do you think?" id="edit"  class="placeholder:italic placeholder:text-slate-400 block border-solid border-2 p-6 border-slate-400  focus:outline-none focus:border-sky-500" name="comment" style="width:400px;"></textarea>
+    <textarea placeholder="What do you think?" id="edit"  class="w-72 placeholder:italic placeholder:text-slate-400 block border-solid border-2 p-6 border-slate-400  focus:outline-none focus:border-sky-500" name="comment" style=""></textarea>
      <a href="" class="p-3 mt-4 bg-black text-white text-md font-bold justify-self-end rounded">Drop</a>
 </div>
 </div>
