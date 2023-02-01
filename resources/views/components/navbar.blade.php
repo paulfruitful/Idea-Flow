05477af5170c4faa104924f69d1979f69e1b78d5
Paul Fruitful <fruitful2007@outlook.com>
2023-02-01 18:37:02 +0100

    Mobile nav done !

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -45,7 +45,7 @@ function nav() {
 </div>
 <!-- Mobile View -->
 <div class="   z-40 absolute  inset-y-0 left-0  flex flex-col lg:hidden sm:hidden md:hidden xl:hidden text-center" id="navbar" style="display:none;">
-  <div class="grid justify-items-center fixed bg-white border-solid shadow-md p-10 m-auto "  style="margin-top:-25px; height:120vh; width:200px;">
+  <div class="grid justify-items-center fixed bg-white border-solid shadow-md p-10 m-auto "  style="margin-top:-25px; height:120vh; width:220px;">
   <a href="/pools/ideas" class="text-md  font-bold p-3 hover:p-4 flex flex-nowrap flex-row hover:text-indigo-500 text-gray-800 " style="margin-top: 10px; margin-bottom:-55px;">
     <svg xmlns="http://www.w3.org/2000/svg" class="svg mr-4 "viewBox="0 0 512 512"><!-- Font Awesome Pro 5.15.4 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) --><path d="M502.63 214.63l-45.25-45.25c-6-6-14.14-9.37-22.63-9.37H384V80c0-26.51-21.49-48-48-48H176c-26.51 0-48 21.49-48 48v80H77.25c-8.49 0-16.62 3.37-22.63 9.37L9.37 214.63c-6 6-9.37 14.14-9.37 22.63V320h128v-16c0-8.84 7.16-16 16-16h32c8.84 0 16 7.16 16 16v16h128v-16c0-8.84 7.16-16 16-16h32c8.84 0 16 7.16 16 16v16h128v-82.75c0-8.48-3.37-16.62-9.37-22.62zM320 160H192V96h128v64zm64 208c0 8.84-7.16 16-16 16h-32c-8.84 0-16-7.16-16-16v-16H192v16c0 8.84-7.16 16-16 16h-32c-8.84 0-16-7.16-16-16v-16H0v96c0 17.67 14.33 32 32 32h448c17.67 0 32-14.33 32-32v-96H384v16z"/></svg>
   <p class="font-bold text-md">Ideas</p>
