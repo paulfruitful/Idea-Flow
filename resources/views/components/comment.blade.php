14db4949d9229689186e35373165b997c519bbca
Paul Fruitful <fruitful2007@outlook.com>
2023-02-04 21:16:08 +0100

    Comment

--- resources/views/components/comment.blade.php
+++ resources/views/components/comment.blade.php
@@ -1,5 +1,5 @@
 @props(['comment'])
-<div class="p-6 w-full flex flex-col">
+<div class="p-6 w-full flex flex-col border-solid border-2 border-gray-500">
 <div class="grid justify-self-stretch">
 <div class="text-center text-md">{{$comment->comment}}</div>
 <div class="justify-self-end">{{$comment->username}}</div>
