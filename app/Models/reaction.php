a20ab928f1f598da454398cbed6f97a031d16054
Paul Fruitful <fruitful2007@outlook.com>
2023-02-04 21:23:17 +0100

    Reaction model and migration + relationships

---
+++ app/Models/reaction.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Idea;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class reaction extends Model
+{
+    use HasFactory;
+    protected $fillable=[
+        'user_id',
+        'idea_id'
+    ];
+    public function Idea(){
+        return $this->belongsTo(Idea::class,'idea_id');
+    }
+}
