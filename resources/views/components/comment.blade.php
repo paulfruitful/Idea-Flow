487fc3498c00d5b49fbc97942d3796eca8467f28
Paul Fruitful <fruitful2007@outlook.com>
2023-02-06 22:52:59 +0100

    Comment list completed

--- resources/views/components/comment.blade.php
+++ resources/views/components/comment.blade.php
@@ -1,7 +1,7 @@
 @props(['comment'])
-<div class="p-6 w-full flex flex-col">
-<div class="grid justify-self-stretch">
+<div class="p-8 w-1/2 border-solid shadow-md justify-self-center flex flex-col">
+<div class="grid">
 <div class="justify-self-start text-md">{{$comment->comment}}</div>
-<div class="justify-self-end">{{$comment->username}}</div>
+<div class="justify-self-end font-bold">- {{$comment->username}}</div>
 </div>
 </div>
--- resources/views/components/comments.blade.php
+++ resources/views/components/comments.blade.php
@@ -1,8 +1,11 @@
 @props(['comments'])
 <div class="p-6 flex flex-col w-full">
+    <p class="text-center p-6 font-bold text-xl">Comments</p>
+<div class="grid mt-3">
 @forelse ($comments as $comment)
     <x-comment :comment="$comment"/>
 @empty
     
 @endforelse
+</div>
 </div>
