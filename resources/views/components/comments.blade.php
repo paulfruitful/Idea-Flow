f6dded85fe43236256efbcd40a5e0d191831000c
Paul Fruitful <fruitful2007@outlook.com>
2023-02-07 21:49:45 +0100

    UI patches

--- resources/views/components/comments.blade.php
+++ resources/views/components/comments.blade.php
@@ -1,11 +1,13 @@
 @props(['comments'])
 <div class="p-6 flex flex-col w-full">
+    
     <p class="text-center p-6 font-bold text-xl">Comments</p>
-<div class="grid mt-3">
+
+    <div class="grid mt-3">
 @forelse ($comments as $comment)
     <x-comment :comment="$comment"/>
 @empty
-    
+    <p class="text-gray-500  text-center text-md ">-- No Comments Yet -- </p>
 @endforelse
 </div>
 </div>
