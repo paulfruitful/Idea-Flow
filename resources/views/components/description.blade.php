41c080654f4d57088856473cbe65bed02631e23e
Paul Fruitful <fruitful2007@outlook.com>
2023-02-07 21:33:52 +0100

    UI patches

--- resources/views/components/description.blade.php
+++ resources/views/components/description.blade.php
@@ -3,7 +3,7 @@
     <div class="text-center mb-3 font-bold text-3xl">Description</div>
  <div class="grid justify-self-stretch">
 <div class="justify-self-end"><p class="text-md ">By <span class="font-bold">{{ucwords($idea->author)}}</span></p></div>
-<div class="justify-self-start"><span class="p-3 shadow-sm bg-black text-white border-solid rounded">{{$idea->plan=="combined"?"A Software X Hardware":'A' .' '. ucwords($idea->plan)}}</span></div> 
+<div class="justify-self-start"><span class="font-bold shadow-sm  text-black ">{{$idea->plan=="combined"?"A Software X Hardware":'A' .' '. ucwords($idea->plan)}}</span></div> 
 </div>
 <div class=" grid mt-4 justify-self-stretch justify-items-center lg:block lg:m-auto">
     <div class="w-full justify-self-center  p-6 lg:block">
