5ed3284b6c06e0661d651506b71f674aa4c8dfa6
Paul Fruitful <fruitful2007@outlook.com>
2023-02-11 23:09:46 +0100

    User to solution relationship

--- app/Models/Solution.php
+++ app/Models/Solution.php
@@ -2,11 +2,18 @@
 
 namespace App\Models;
 
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 
 class Solution extends Model
 {
     use HasFactory,HasUuids;
+ protected $fillable=[];
+
+ public function user(){
+    return $this->belongsToMany(User::class,'user_id');
+ }
+
 }
