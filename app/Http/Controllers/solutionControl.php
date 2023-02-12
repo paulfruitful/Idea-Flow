3a41f46029dee4b168a51541a5491d00710dabe9
Paul Fruitful <fruitful2007@outlook.com>
2023-02-12 23:13:52 +0100

    List all function; solution pools

--- app/Http/Controllers/solutionControl.php
+++ app/Http/Controllers/solutionControl.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Solution;
 use Illuminate\Http\Request;
 
 class solutionControl extends Controller
 {
     //
-    
+    public function all(){
+        
+        return view('pool.solution',[
+            'ideas'=>Solution::latest()->paginate(10)
+        ]);
+    }
 }
