bff8d0c669166cf13bdc3db868b56c09013947e7
Paul Fruitful <fruitful2007@outlook.com>
2023-02-13 12:39:12 +0100

    Solution comment migration

--- database/migrations/2023_02_13_113428_create_solution_comments_table.php
+++ database/migrations/2023_02_13_113428_create_solution_comments_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('solution_comments', function (Blueprint $table) {
             $table->id();
+            
+            $table->string('username');
+            $table->longText('comment');
+            $table->foreignId('idea_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
