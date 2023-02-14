fc42c537a69e809568bd6263d9da1d56682f2d61
Paul Fruitful <fruitful2007@outlook.com>
2023-02-14 18:32:31 +0100

    Solution Factory

--- database/factories/solutionFactory.php
+++ database/factories/solutionFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Solution;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -14,10 +15,21 @@ class solutionFactory extends Factory
      *
      * @return array<string, mixed>
      */
+    protected $model=Solution::class;
     public function definition()
     {
         return [
             //
+            'title'=>fake()->company(),
+            'description'=>implode('', fake()->sentences(10)),
+            'tagline'=>fake()->sentence(),
+            'email'=>fake()->unique()->safeEmail(),
+            'plan'=>fake()->randomElement(['hardware','software']),
+            'sponsor'=>fake()->randomElement(['true','false']),
+            'privacy'=>fake()->randomElement(['true','false']),
+            'upvote'=>fake()->numberBetween(5,100),
+          
+
         ];
     }
 }
