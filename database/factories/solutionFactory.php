bc97eb11ec9636bf6185ed1d7d893e85a6a97437
Paul Fruitful <fruitful2007@outlook.com>
2023-02-14 18:30:39 +0100

    Solution Factory

---
+++ database/factories/solutionFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\solution>
+ */
+class solutionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
