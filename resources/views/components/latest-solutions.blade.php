1249af818f16a21b6dd1f46274c13fff61f27d8b
Paul Fruitful <fruitful2007@outlook.com>
2023-02-14 19:23:47 +0100

    Latest solutions block comp

--- resources/views/components/latest-solutions.blade.php
+++ resources/views/components/latest-solutions.blade.php
@@ -1,7 +1,7 @@
 @props(['solutions'])
 
 <div class="flex flex-col lg:flex-col flex-wrap w-full h-full p-6 ">
- @forelse ($solutions as $idea)
+ @forelse ($solutions as $solution)
  <x-solution :solution="$solution"/>
  @empty
      <p>No Idea</p>
