fa70941317eedd3527af6c4faf1398234d3c0678
Paul Fruitful <fruitful2007@outlook.com>
2023-02-20 13:09:48 +0100

    A mini api for react

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Idea;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/ideas',function(){
+   return json_encode(Idea::all());
 });
