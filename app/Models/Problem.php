711758efd0d93a6275c11cfc5f4e3ba24062aea0
Paul Fruitful <fruitful2007@outlook.com>
2023-02-21 21:50:56 +0100

    Correcions

--- app/Models/Problem.php
+++ app/Models/Problem.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\ProblemComments;
+use App\Models\ProblemReactions;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
