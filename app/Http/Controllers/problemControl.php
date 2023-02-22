9c45e02213d38fac93dd00525738209d5acb18a3
Paul Fruitful <fruitful2007@outlook.com>
2023-02-22 23:56:12 +0100

    Problem single view

--- app/Http/Controllers/problemControl.php
+++ app/Http/Controllers/problemControl.php
@@ -15,4 +15,46 @@ class problemControl extends Controller
         ]);
     }
     
+    public function problem(Problem $problem){
+    
+        
+        $check_id=$problem->check_reaction(); //Returns your reaction
+
+        //Checks if your reaction is greater than 0
+        if(count($check_id)>0){
+            
+            //Checks whether the image is locally stored
+        if(preg_match('/images/i',$problem->image)){
+            return view('problem.problem',[
+            'idea'=>$problem,
+            'liked'=>true, //Returns true if $check_id is greater than 0
+            'unliked'=>false, //Returns false if $check_id is  equal 0
+            "image"=>"/storage/".$problem->image //Returns this if the storage is local
+        ]);
+    }else{
+        return view('problem.problem',[
+            'idea'=>$problem,
+            'liked'=>true,
+            'unliked'=>false,
+            "image"=>$problem->image
+        ]);
+    }
+    }else{
+        if(preg_match('/images/i',$problem->image)){
+        return view('problem.problem',[
+            'idea'=>$problem,
+            'liked'=>false,
+            'unliked'=>true,
+            'image'=>"/storage/".$problem->image
+        ]);
+    }else{
+        return view('problem.problem',[
+            'idea'=>$problem,
+            'liked'=>false,
+            'unliked'=>true,
+            "image"=>$problem->image
+        ]);
+    }
+    }
+    }
 }
