e68b630d36a379f8ed527537ba25cc8a6f12239e
Paul Fruitful <fruitful2007@outlook.com>
2023-02-22 23:28:21 +0100

    Problem pool

--- app/Http/Controllers/problemControl.php
+++ app/Http/Controllers/problemControl.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Problem;
 use Illuminate\Http\Request;
 
 class problemControl extends Controller
 {
     //
+    public function all(){
+   
+        return view('pool.problem',[
+            'problem'=>Problem::recent(),
+        ]);
+    }
+    
 }
