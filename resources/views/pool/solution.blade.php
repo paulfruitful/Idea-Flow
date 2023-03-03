ccdf431b9b719b515a263968ec26f93fdea4f3cb
Paul Fruitful <fruitful2007@outlook.com>
2023-03-04 00:00:13 +0100

    ui edit

--- resources/views/pool/solution.blade.php
+++ resources/views/pool/solution.blade.php
@@ -11,11 +11,11 @@
  
     <x-navbar/>
     <div class="block">
-    <div class="grid justify-self-stretch justify-items-center mt-3 mb-6  bg-gradient-to-r from-cyan-500 to-blue-700 w-full" style="height: 400px">
+    <div class="grid justify-self-stretch justify-items-center mt-3 mb-6  bg-gradient-to-r from-green-400 to-green-800 w-full" style="height: 400px">
       <div class=" justify-self-center m-auto">
       <div class="m-auto flex flex-col">
       <p class="text-2xl lg:text-5xl text-white m-auto font-bold "><span class="">Discover Solutions</span> </p>
-      <p class="text-xl font-bold m-auto p-6 mb-4" style="color: #ffffff">Solve Your Problems</p>
+      <p class="text-2xl italic m-auto p-6 mb-4" style="color: #ffffff">Solve Your Problems</p>
       <div class="justify-items-center flex flex-row m-auto">
      <a href="/create/solution" class="p-4 m-auto hover:shadow-md hover:p-3 text-md text-gray-800 flex flex-row font-bold border-solid shadow-xl rounded-md bg-black text-white" >New Solution</a>
       
