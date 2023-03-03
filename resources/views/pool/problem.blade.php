2e4c260f7c6bf28cdd3a854fd5cfb7bcca966497
Paul Fruitful <fruitful2007@outlook.com>
2023-03-03 23:54:16 +0100

    ui edit

--- resources/views/pool/problem.blade.php
+++ resources/views/pool/problem.blade.php
@@ -11,13 +11,13 @@
  
     <x-navbar/>
     <div class="block">
-    <div class="grid justify-self-stretch justify-items-center mt-3 mb-6  bg-gradient-to-r from-cyan-500 to-blue-700 w-full" style="height: 400px">
+    <div class="grid justify-self-stretch justify-items-center mt-3 mb-6  bg-gradient-to-r from-red-500 to-red-700 w-full" style="height: 400px">
       <div class=" justify-self-center m-auto">
       <div class="m-auto flex flex-col">
       <p class="text-3xl lg:text-5xl text-white m-auto font-bold "><span class="">Discover Problems</span> </p>
-      <p class="text-md lg:text-2xl font-bold m-auto p-6 mb-4" style="color: #ffffff">Create Ideas</p>
+      <p class="text-md lg:text-2xl  m-auto p-6 mb-4" style="color: #ffffff">And Create Ideas</p>
       <div class="justify-items-center flex flex-row m-auto">
-     <a href="/create/idea" class="p-4 m-auto hover:shadow-md hover:p-3 text-md text-gray-800 flex flex-row font-bold border-solid shadow-xl rounded-md bg-black text-white" >New Idea</a>
+     <a href="/create/idea" class="p-4 m-auto hover:shadow-md hover:p-3 text-md  flex flex-row font-bold border-solid shadow-xl rounded-md bg-white text-black" >New Idea</a>
       
       </div>
       </div>
