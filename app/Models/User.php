aa57965582818144698fda5ffe2b7632f646e56e
Paul Fruitful <fruitful2007@outlook.com>
2023-03-14 22:26:41 +0100

    Passport for Oauth setup

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,12 +6,12 @@ namespace App\Models;
 use App\Models\Idea;
 use App\Models\Problem;
 use App\Models\Solution;
-use Laravel\Sanctum\HasApiTokens;
+//use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-
+use Laravel\Passport\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable,HasUuids;
