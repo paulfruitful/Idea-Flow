2ce6477c028986f66b4e777a2b5dd05cba8ee56e
Paul Fruitful <fruitful2007@outlook.com>
2023-03-20 02:53:30 +0100

    FInal moments

--- resources/views/pool/problem.blade.php
+++ resources/views/pool/problem.blade.php
@@ -18,7 +18,7 @@
       <p class="text-md lg:text-2xl  m-auto p-6 mb-4" style="color: #ffffff">And Create Ideas</p>
       <div class="justify-items-center flex flex-row m-auto">
      <a href="/create/problem" class="p-4 m-auto hover:shadow-md hover:p-3 text-md  flex flex-row font-bold border-solid shadow-xl rounded-md bg-white text-black" >   <svg style="width:15px; height:20px;" class="mt-1 ml-1 mr-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!-- Font Awesome Pro 5.15.4 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) --><path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/></svg>
-      New Idea</a>
+      New Problem</a>
       
       </div>
       </div>
