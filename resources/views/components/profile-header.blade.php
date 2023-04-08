c7699aae289b7ea9a41d0ba0b8bea437c552875f
Paul Fruitful <fruitful2007@outlook.com>
2023-04-08 19:33:54 +0100

    Profile Photo section

--- resources/views/components/profile-header.blade.php
+++ resources/views/components/profile-header.blade.php
@@ -1,13 +1,13 @@
 <div class="grid p-8 relative w-full">
 <!--Cover Photo-->
 <div class="grid justify-self-stretch mb-6 relative p-6 w-full">
-    <img src={{auth()->user()->cover_photo?auth()->user()->cover_photo:'demo pic'}} alt="" class="p-6 relative">
+    <img src={{auth()->user()->cover_photo?auth()->user()->cover_photo:'d emo pic'}} alt="" class="p-6 relative">
 
 </div>
 
-
-<div class="z-50 justify-self-start justify-items-center rounded">
-
+<!--Profile Photo-->
+<div class="z-50 justify-self-start justify-items-center p-6">
+   <img src={{auth()->user()->avatar?auth()->user()->avatar:'d emo pic'}}  class="p-6 justify-self-start rounded-lg z-50">
 
 </div>
 
