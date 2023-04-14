552ec2f168f8f32f887321d4c67cab649a8ac8d2
Paul Fruitful <fruitful2007@outlook.com>
2023-04-14 23:17:35 +0100

    Tags for solutions

--- resources/views/components/solution-tags.php
+++ resources/views/components/solution-tags.php
@@ -1,8 +1,8 @@
 <div class="flex flex-wrap p-6">
-  <a href="/?tag="" "></a><li class="lg:p-3 p-2 m-4 lg:text-md text-sm bg-white text-black shadow-sm list-none mr-6 border-2 hover:p-4 focus:p-4 border-solid border-black rounded">Health</li>
-  <a href="/?tag="" "></a><li class="lg:p-3 p-2 m-4 lg:text-md text-sm bg-white text-black shadow-sm list-none mr-6 border-2 hover:p-4 focus:p-4 border-solid border-black rounded">Finance</li>
-  <a href="/?tag="" "></a><li class="lg:p-3 p-2 m-4 lg:text-md text-sm bg-white text-black shadow-sm list-none mr-6 border-2 hover:p-4 focus:p-4 border-solid border-black rounded">Agriculture</li>
-  <a href="/?tag="" "></a><li class="lg:p-3 p-2 m-4 lg:text-md text-sm bg-white text-black shadow-sm list-none mr-6 border-2 hover:p-4 focus:p-4 border-solid border-black rounded">Fashion</li>
-  <a href="/?tag="" "></a><li class="lg:p-3 p-2 m-4 lg:text-md text-sm bg-white text-black shadow-sm list-none mr-6 border-2 hover:p-4 focus:p-4 border-solid border-black rounded">Entertainment</li>
+  <a href="/pools/solutions/?tag= "></a><li class="lg:p-3 p-2 m-4 lg:text-md text-sm bg-white text-black shadow-sm list-none mr-6 border-2 hover:p-4 focus:p-4 border-solid border-black rounded">Health</li>
+  <a href="/pools/solutions/?tag= "></a><li class="lg:p-3 p-2 m-4 lg:text-md text-sm bg-white text-black shadow-sm list-none mr-6 border-2 hover:p-4 focus:p-4 border-solid border-black rounded">Finance</li>
+  <a href="/pools/solutions/?tag= "></a><li class="lg:p-3 p-2 m-4 lg:text-md text-sm bg-white text-black shadow-sm list-none mr-6 border-2 hover:p-4 focus:p-4 border-solid border-black rounded">Agriculture</li>
+  <a href="/pools/solutions/?tag= "></a><li class="lg:p-3 p-2 m-4 lg:text-md text-sm bg-white text-black shadow-sm list-none mr-6 border-2 hover:p-4 focus:p-4 border-solid border-black rounded">Fashion</li>
+  <a href="/pools/solutions/?tag= "></a><li class="lg:p-3 p-2 m-4 lg:text-md text-sm bg-white text-black shadow-sm list-none mr-6 border-2 hover:p-4 focus:p-4 border-solid border-black rounded">Entertainment</li>
 
 </div>
