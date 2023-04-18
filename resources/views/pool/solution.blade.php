f078b04d776b53b28305a65ccf4876c26c058176
Paul Fruitful <fruitful2007@outlook.com>
2023-04-18 21:02:48 +0100

    Tags for solutions

--- resources/views/pool/solution.blade.php
+++ resources/views/pool/solution.blade.php
@@ -6,6 +6,13 @@
   <link rel="icon" type="image/x-icon" href="fan-solid.svg">
   <title>IdeaFlow</title>
   <script src="https://cdn.tailwindcss.com"></script>
+  <style>
+    .svg{
+      height:20px ;
+      width: 25px;
+    }
+   
+  </style>
 </head>
 <body class="block">
  
@@ -23,10 +30,17 @@
       </div>
     </div>
   -->
-  <div class="grid justify-self-stretch">
-    <div class="flex">
+  <div class="flex lg:flex-row flex-col lg:flex-row-reverse justify-between w-full">     
+         <div class=" relative order-first m-6">
+          <div class="flex block border-solid border-t-0 border-b-2 border-black m-2 ">
+          <svg xmlns="http://www.w3.org/2000/svg" class="svg" viewBox="0 0 512 512"><!-- Font Awesome Pro 5.15.4 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) --><path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"/></svg><input type="text" placeholder="Search Solutions" class="placeholder:text-slate-400   focus:outline-none  focus:border-sky-500">
+         </div>
+        </div>
+  
       <x-solution-tags/>
     </div>
+
+  
   <div class="text-center m-6 mt-8 font-bold text-4xl">Solution Pool</div>
   
   
