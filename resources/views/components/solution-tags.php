5f592d1ed9c3746d7400c7b4b462d1bdec74053d
Paul Fruitful <fruitful2007@outlook.com>
2023-04-18 20:06:52 +0100

    Tags for solutions

--- resources/views/components/solution-tags.php
+++ resources/views/components/solution-tags.php
@@ -1,8 +1,8 @@
 <div class="flex flex-wrap p-6">
-  <a href="/pools/solutions?tag=Health "><li class="lg:p-3 p-2 m-4 lg:text-md text-sm bg-white text-black shadow-sm list-none mr-6 border-2 hover:p-4 focus:p-4 border-solid border-black rounded">Health</li></a>
-  <a href="/pools/solutions?tag=Finance "><li class="lg:p-3 p-2 m-4 lg:text-md text-sm bg-white text-black shadow-sm list-none mr-6 border-2 hover:p-4 focus:p-4 border-solid border-black rounded">Finance</li></a>
-  <a href="/pools/solutions?tag=Agriculture "><li class="lg:p-3 p-2 m-4 lg:text-md text-sm bg-white text-black shadow-sm list-none mr-6 border-2 hover:p-4 focus:p-4 border-solid border-black rounded">Agriculture</li></a>
-  <a href="/pools/solutions?tag=Fashion "><li class="lg:p-3 p-2 m-4 lg:text-md text-sm bg-white text-black shadow-sm list-none mr-6 border-2 hover:p-4 focus:p-4 border-solid border-black rounded">Fashion</li></a>
-  <a href="/pools/solutions?tag=Entertainment "><li class="lg:p-3 p-2 m-4 lg:text-md text-sm bg-white text-black shadow-sm list-none mr-6 border-2 hover:p-4 focus:p-4 border-solid border-black rounded">Entertainment</li></a>
+  <a href="/pools/solutions?tag=Health "><li class="p-1  m-4 lg:text-md text-sm bg-white text-black shadow-sm list-none mr-6 border-2 hover:p-2 focus:p-4 border-solid border-black rounded">Health</li></a>
+  <a href="/pools/solutions?tag=Finance "><li class="p-1  m-4 lg:text-md text-sm bg-white text-black shadow-sm list-none mr-6 border-2 hover:p-2 focus:p-4 border-solid border-black rounded">Finance</li></a>
+  <a href="/pools/solutions?tag=Agriculture "><li class="p-1 m-4 lg:text-md text-sm bg-white text-black shadow-sm list-none mr-6 border-2 hover:p-2 focus:p-4 border-solid border-black rounded">Agriculture</li></a>
+  <a href="/pools/solutions?tag=Fashion "><li class="p-1 m-4 lg:text-md text-sm bg-white text-black shadow-sm list-none mr-6 border-2 hover:p-2 focus:p-4 border-solid border-black rounded">Fashion</li></a>
+  <a href="/pools/solutions?tag=Entertainment "><li class="p-1 m-4 lg:text-md text-sm bg-white text-black shadow-sm list-none mr-6 border-2 hover:p-2 focus:p-4 border-solid border-black rounded">Entertainment</li></a>
 
 </div>
