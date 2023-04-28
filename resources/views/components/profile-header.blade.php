19b7380ef7cdf1e46197f2d52e538fac6671a893
Paul Fruitful <fruitful2007@outlook.com>
2023-04-28 15:11:18 +0100

    Profile

--- resources/views/components/profile-header.blade.php
+++ resources/views/components/profile-header.blade.php
@@ -1,9 +1,5 @@
 <div class="grid p-8 relative w-full">
-<!--Cover Photo-->
-<div class="grid justify-self-stretch mb-6 relative p-6 w-full">
-    <img src={{auth()->user()->cover_photo?auth()->user()->cover_photo:'https://th.bing.com/th/id/R.be2d887f773575b6bac522a262fa484e?rik=1Xo%2fiBKoNeHC%2fA&pid=ImgRaw&r=0'}} alt="" class="p-6 relative">
 
-</div>
 
 <!--Profile Photo-->
 <div class="rounded-full border-solid  z-40 lg:m-16 absolute m-6 justify-self-start justify-items-center bg-gray-200 ">
