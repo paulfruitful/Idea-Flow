a85b2e7cf66be0d2b07b0fdd41b34744497cd134
Paul Fruitful <fruitful2007@outlook.com>
2023-04-28 15:16:12 +0100

    Profile

--- resources/views/components/profile-header.blade.php
+++ resources/views/components/profile-header.blade.php
@@ -1,10 +1,10 @@
-<div class="grid p-8 relative w-full">
+<div class="grid p-8 justify-self-stretch relative w-full">
 
 
 <!--Profile Photo-->
-<div class="rounded-full border-solid  z-40 lg:m-16 absolute m-6 justify-self-start justify-items-center bg-gray-200 ">
+<div class="rounded-full border-solid  z-40 lg:m-16 absolute m-6 justify-self-start justify-items-center ">
    <img class="p-6 justify-self-start rounded-lg z-2" height="200" width="150" src={{auth()->user()->avatar?auth()->user()->avatar:'https://i.ibb.co/KLLtzGc/avatar-1.png'}} >
-
+   <span class="font-bold text-2xl m-3">{{auth()->user()->name}}</span>
 </div>
 
 
