cbcd40dea5a3a9d977a83800285240b9e091a32b
Paul Fruitful <fruitful2007@outlook.com>
2023-04-29 19:24:03 +0100

    Ui edits for Dashboard

--- resources/views/components/profile-header.blade.php
+++ resources/views/components/profile-header.blade.php
@@ -7,12 +7,14 @@
 
 
 <!--Profile Photo-->
-<div class="rounded-full border-solid z-40 lg:m-16 m-6 justify-self-center  justify-items-center ">
+<div class=" rounded-full border-solid z-40 lg:m-16 m-6 justify-self-center  justify-items-center ">
    <img class="p-6 justify-self-center lg:p-3 bg-white shadow-lg rounded-full m-3 z-2" height="200" width="150" src={{auth()->user()->avatar?auth()->user()->avatar:'https://i.ibb.co/KLLtzGc/avatar-1.png'}} >
+   <div class="flex flex-col">
    <span class="font-bold text-white text-2xl lg:text-3xl lg:m-3 m-6">{{auth()->user()->name}}</span>
    @if (auth()->user()->username==request()->user)
-     <span class="text-md p-6 font-bold italic">Followers: <span class="not-italic p-3">{{}}</span></span>       
+     <span class="text-md text-white p-6 font-bold italic">Followers: <span class="not-italic p-3">{{auth()->user()->followers?auth()->user()->followers:0}}</span></span>       
    @endif
+</div>
 </div>
 
 
