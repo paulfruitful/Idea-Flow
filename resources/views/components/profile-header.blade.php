6e298482c8336d8c6c0c0a7ebae10795f6f38924
Paul Fruitful <fruitful2007@outlook.com>
2023-04-29 19:15:16 +0100

    Ui edits for Dashboard

--- resources/views/components/profile-header.blade.php
+++ resources/views/components/profile-header.blade.php
@@ -8,7 +8,7 @@
 
 <!--Profile Photo-->
 <div class="rounded-full border-solid z-40 lg:m-16 m-6 justify-self-center  justify-items-center ">
-   <img class="p-6 justify-self-center  bg-white shadow-lg rounded-full m-6 z-2" height="200" width="150" src={{auth()->user()->avatar?auth()->user()->avatar:'https://i.ibb.co/KLLtzGc/avatar-1.png'}} >
+   <img class="p-6 justify-self-center lg:p-3 bg-white shadow-lg rounded-full m-3 z-2" height="200" width="150" src={{auth()->user()->avatar?auth()->user()->avatar:'https://i.ibb.co/KLLtzGc/avatar-1.png'}} >
    <span class="font-bold text-white text-2xl lg:text-3xl lg:m-3 m-6">{{auth()->user()->name}}</span>
 </div>
 
