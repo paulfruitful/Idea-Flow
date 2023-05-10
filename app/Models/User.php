d01af98aa32802eb72a275db98f781ac210927cc
Paul Fruitful <fruitful2007@outlook.com>
2023-05-10 20:55:38 +0100

    Follower-User Relationships

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,13 +5,15 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use App\Models\Idea;
 use App\Models\Problem;
-use App\Models\Solution;
+use App\Models\Follower;
 //use Laravel\Sanctum\HasApiTokens;
+use App\Models\Solution;
+use Laravel\Passport\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Laravel\Passport\HasApiTokens;
+
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable,HasUuids;
