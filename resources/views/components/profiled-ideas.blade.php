f2cb457f49c6a490fb738e723a40e8b2bd2c8fdf
Paul Fruitful <fruitful2007@outlook.com>
2023-05-10 06:32:37 +0100

    User Ideas: Component, heading div

--- resources/views/components/profiled-ideas.blade.php
+++ resources/views/components/profiled-ideas.blade.php
@@ -1,4 +1,6 @@
 @props(['user'])
 <div class="grid w-full p-6 ">
-
+<div class="grid justify-self-center justify-items-center p-6">
+    <p class="text-2xl font-bold italics">{{$user->name}}</p>
+</div>
 </div>
