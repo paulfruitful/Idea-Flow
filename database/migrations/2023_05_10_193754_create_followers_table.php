008ce25701ecdaa5fc61fc1a83d9fc86db01e296
Paul Fruitful <fruitful2007@outlook.com>
2023-05-10 20:52:09 +0100

    Followers migration

--- database/migrations/2023_05_10_193754_create_followers_table.php
+++ database/migrations/2023_05_10_193754_create_followers_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('followers', function (Blueprint $table) {
             $table->id();
             $table->string('follower_id');
-            $table->foreignUuid('user_id');
+            $table->foreignUuid('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
