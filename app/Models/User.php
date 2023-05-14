e859ad403de5dae6659146f570ccd4184c9a0566
Paul Fruitful <fruitful2007@outlook.com>
2023-05-14 16:16:13 +0100

    Error found

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,7 +51,7 @@ public function followers(){
 }
 
 public function check_followers(){
-  return $this->followers->where('user_id', auth()->id());
+  return $this->followers->where('follower_id', auth()->id());
 }
     /**
      * The attributes that should be hidden for serialization.
