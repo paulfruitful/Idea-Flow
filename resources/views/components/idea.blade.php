cee18e7b45c9a19fe8070727d21f7a06790805d4
Paul Fruitful <fruitful2007@outlook.com>
2023-05-15 22:46:43 +0100

    Is_user for Ideas comp

--- resources/views/components/idea.blade.php
+++ resources/views/components/idea.blade.php
@@ -1,4 +1,20 @@
-@props(['idea'])
+@props(['idea','is_user'])
+@if ($is_user)
+<a href="/ideas/{{$idea->id}}" class="">
+    <div class="block p-6 relative bg-white hover:shadow-lg hover:p-8 text-black shadow-md m-4">
+        <div class="text-center flex flex-row flex-row-reverse p-6 text-blue-600 text-md" style="font-size: 20px;"><img src="https://cdn-icons-png.flaticon.com/512/9135/9135999.png" class="" style="height: 30px; width:30px; margin-top:-5px;" alt=""></div>
+    
+    <div class="text-center font-bold p-6 text-xl">-{{$idea->title}}</div>
+    <div class="relative grid">
+    <div class="relative text-md mb-3 justify-self-start">By <a href="/ideas/{{$idea->id}}" class=" font-bold">You</a></div>
+    <div class=" relative justify-self-end ">
+    <a class="flex flex-row hover:text-blue-600 hover:px-3 " href="/ideas/{{$idea->id}}">Learn more <img src="https://cdn-icons-png.flaticon.com/512/2989/2989988.png" style="height: 30px; width:30px;" alt=""> </a>
+    </div>
+    </div>
+    </div>
+    </a>
+     
+@endif
 <a href="/ideas/{{$idea->id}}" class="">
 <div class="block p-6 relative bg-white hover:shadow-lg hover:p-8 text-black shadow-md m-4">
     <div class="text-center flex flex-row flex-row-reverse p-6 text-blue-600 text-md" style="font-size: 20px;"><img src="https://cdn-icons-png.flaticon.com/512/9135/9135999.png" class="" style="height: 30px; width:30px; margin-top:-5px;" alt=""><p class="mr-2">{{$idea->tagline}} </p></div>
