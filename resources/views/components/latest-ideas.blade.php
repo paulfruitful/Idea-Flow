ae920b579a6f76fd7fa63cc041b22af52baa5dcb
Paul Fruitful <fruitful2007@outlook.com>
2023-05-15 22:58:11 +0100

    User Ideas

--- resources/views/components/latest-ideas.blade.php
+++ resources/views/components/latest-ideas.blade.php
@@ -1,8 +1,8 @@
-@props(['ideas'])
+@props(['ideas','is_user'])
 
 <div class="flex flex-col  flex-wrap w-full h-full p-6 ">
  @forelse ($ideas as $idea)
- <x-idea :idea="$idea"/>
+ <x-idea :idea="$idea" :is_user="$is_user"/>
  @empty
      
  <p class="text-center text-gray-500 text-xl">--No Ideas--</p>
