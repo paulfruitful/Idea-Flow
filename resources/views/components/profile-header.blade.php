d75a659ea81fe6866f8201daccdff8e025de81dc
Paul Fruitful <fruitful2007@outlook.com>
2023-05-16 20:15:08 +0100

    Z index correct

--- resources/views/components/profile-header.blade.php
+++ resources/views/components/profile-header.blade.php
@@ -7,8 +7,8 @@
 
 
 <!--Profile Photo-->
-<div class=" rounded-full border-solid z-40 lg:m-16 m-6 justify-self-center  justify-items-center ">
-   <img class="p-6 justify-self-center lg:p-3 bg-white shadow-lg rounded-full ml-4 lg:ml-6 relative  z-2" height="200" width="150" src={{auth()->user()->avatar?auth()->user()->avatar:'https://i.ibb.co/KLLtzGc/avatar-1.png'}} >
+<div class=" rounded-full border-solid  lg:m-16 m-6 justify-self-center  justify-items-center ">
+   <img class="p-6 justify-self-center lg:p-3 bg-white shadow-lg rounded-full ml-4 lg:ml-6 relative  " height="200" width="150" src={{auth()->user()->avatar?auth()->user()->avatar:'https://i.ibb.co/KLLtzGc/avatar-1.png'}} >
    <div class="flex flex-col justify-self-center relative">
    <span class="font-bold text-white text-2xl lg:text-3xl lg:m-3 m-4">{{$user->name}}</span>
     <span class="text-md lg:text-xl text-white lg:mx-12 mx-12  italic">Followers: <span class="not-italic">{{auth()->user()->followers?auth()->user()->followers:0}}</span></span>       
