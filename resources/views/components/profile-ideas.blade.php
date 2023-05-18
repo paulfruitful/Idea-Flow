480cf4ea44d097ed4e5a96aec2d6f2d1b47ca50f
Paul Fruitful <fruitful2007@outlook.com>
2023-05-18 21:28:10 +0100

    **

--- resources/views/components/profile-ideas.blade.php
+++ resources/views/components/profile-ideas.blade.php
@@ -3,17 +3,19 @@
 <div class="grid justify-self-center justify-items-center p-6">
     <p class="text-2xl font-bold italics">Ideas</p>
 </div>
-
+@php
+    $ideas=$user->idea->paginate(5)
+@endphp
 
 <div class="flex flex-col  flex-wrap w-full h-full p-3 ">
- @forelse ($user->idea as $idea)
+ @forelse ($ideas as $idea)
  <x-user-idea :idea="$idea" :is_user="$is_user"/>
  @empty
      
  <p class="text-center text-gray-500 text-xl">--No Ideas--</p>
  @endforelse
 
-
+<div class="p-6">{{$ideas->link()}}</div>
 </div>
 </div>
 
