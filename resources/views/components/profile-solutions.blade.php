f4f5dbd52d886c2db67107c21a94f2bfc5affc00
Paul Fruitful <fruitful2007@outlook.com>
2023-05-21 19:39:26 +0100

    /=./

---
+++ resources/views/components/profile-solutions.blade.php
@@ -0,0 +1,20 @@
+@props(['user','is_user','solutions'])
+<div class="grid w-full p-6 ">
+<div class="grid justify-self-center justify-items-center p-6">
+    <p class="text-2xl font-bold italics">Ideas</p>
+</div>
+
+
+<div class="flex flex-col  flex-wrap w-full h-full p-3 ">
+ @forelse ($solutions as $solution)
+ <x-user-idea :idea="$solution" :is_user="$is_user"/>
+ @empty
+     
+ <p class="text-center text-gray-500 text-xl">--No Ideas--</p>
+ @endforelse
+
+</div>
+</div>
+
+
+
