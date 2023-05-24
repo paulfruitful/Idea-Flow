4a7cbc801c92a6d3cf816f4e20791035aa2d7bf2
Paul Fruitful <fruitful2007@outlook.com>
2023-05-24 21:04:11 +0100

    Modal revertion

--- resources/views/components/user-idea.blade.php
+++ resources/views/components/user-idea.blade.php
@@ -40,7 +40,7 @@
      <form method="dialog" id="btn">
         <div class="text-center flex flex-row flex-row-reverse p-6 text-blue-600 text-md" style="font-size: 20px;">
           
-          <button class="bg-transparent border-none"> 
+          <button class="bg-transparent border-none" type="submit"> 
              <svg xmlns="http://www.w3.org/2000/svg" style="height: 30px; width:30px; margin-top:-5px;" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M8 256a56 56 0 1 1 112 0A56 56 0 1 1 8 256zm160 0a56 56 0 1 1 112 0 56 56 0 1 1 -112 0zm216-56a56 56 0 1 1 0 112 56 56 0 1 1 0-112z"/></svg>
        </button> 
    
