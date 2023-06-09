31faa61b58e0c8d8cdccf30ed6c5912f1deeaaac
Paul Fruitful <fruitful2007@outlook.com>
2023-06-09 23:28:10 +0100

    pool  hero wrap up

--- resources/views/components/pool-hero.blade.php
+++ resources/views/components/pool-hero.blade.php
@@ -9,8 +9,8 @@
 <li class="italic  text-lg">Report Your Problems</li>
 </ul>
 <div class="justify-items-center flex flex-row m-auto">
-<a href="/create/idea" class="p-4 mr-4 m-auto hover:shadow-md hover:p-3 text-md text-gray-800 flex flex-row font-bold border-solid shadow-xl rounded-md bg-black text-white" style=""> New Idea</a>
-<a href="/create/idea" class="p-4 m-auto hover:shadow-md hover:p-3 text-md text-gray-800 flex flex-row font-bold border-solid shadow-xl rounded-md bg-white text-black" style="">
+<a href="/create/idea" class="p-4 mr-4 m-auto hover:shadow-md hover:p-3 text-md text-gray-800  flex-row font-bold border-solid shadow-xl rounded-md bg-black text-white" style=""><span class="text-white text-3xl font-bold">+</span> <span class="text-center">New Idea</span></a>
+<a href="/create/solution" class="p-5 m-auto hover:shadow-md hover:p-3 text-md text-gray-800 flex flex-row font-bold border-solid shadow-xl rounded-md bg-white text-black" style="">
     <svg style="width:15px; height:20px;" class="mt-1 ml-1 mr-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!-- Font Awesome Pro 5.15.4 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) --><path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/></svg>
     Solution</a>
 
