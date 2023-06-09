a4917387aff184752717deb91e3ffd68f4019da9
Paul Fruitful <fruitful2007@outlook.com>
2023-06-09 23:14:16 +0100

    Ui changes

--- resources/views/components/pool-hero.blade.php
+++ resources/views/components/pool-hero.blade.php
@@ -2,7 +2,7 @@
 <div class=" justify-self-center m-auto">
 <div class="m-auto flex flex-col">
 <p class="text-3xl lg:text-5xl text-white m-auto font-bold "><span class="">Let Your</span>  Ideas Flow </p>
-<p class="text-xl text-center lg:flex lg:flex-row   p-6 mb-4" style="color: #ffffff">Share Your Ideas <span class="flex-col lg:flex-row">, <span  class=""> Offer Your Solutions, and</span> <span> Report Your Problems</span> </span> </p>
+<p class="text-xl text-center lg:flex lg:flex-row   p-6 mb-4" style="color: #ffffff">Share Your Ideas <span class="flex-col lg:flex-row">, <span  class="p-3 lg:p-0 "> Offer Your Solutions, and</span> <span class="p-3 lg:p-0 "> Report Your Problems</span> </span> </p>
 <div class="justify-items-center flex flex-row m-auto">
 <a href="/create/idea" class="p-4 mr-4 m-auto hover:shadow-md hover:p-3 text-md text-gray-800 flex flex-row font-bold border-solid shadow-xl rounded-md bg-black text-white" style=""> New Idea</a>
 <a href="/create/idea" class="p-4 m-auto hover:shadow-md hover:p-3 text-md text-gray-800 flex flex-row font-bold border-solid shadow-xl rounded-md bg-white text-black" style="">
