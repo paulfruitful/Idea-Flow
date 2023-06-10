b5f2d97238b928792e8623a329017a3f334977af
Paul Fruitful <fruitful2007@outlook.com>
2023-06-10 10:27:53 +0100

    Solution float btn +

--- resources/views/components/solution-float.blade.php
+++ resources/views/components/solution-float.blade.php
@@ -3,7 +3,7 @@
 
     <!-- Implement the FAB -->
 
-    <a href="/create/solution"  class="fixed z-90 bottom-10 right-2  w-20 h-20 rounded-full drop-shadow-lg flex justify-center bg-green-700 shadow-md items-center text-white text-5xl hover:bg-gray-300 hover:drop-shadow-2xl hover:animate-bounce duration-300">
+    <a href="/create/solution"  class="fixed z-90 bottom-10 right-2  w-20 h-20 rounded-full drop-shadow-lg flex justify-center bg-green-700 shadow-md items-center text-white text-5xl hover:bg-gray-500 hover:drop-shadow-2xl hover:animate-bounce duration-300">
        +</a>
 
     <!-- Javascript code -->
