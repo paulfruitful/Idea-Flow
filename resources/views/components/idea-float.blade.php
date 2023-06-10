544685e2c1c3106aa1956b36636fddaac51d2bfd
Paul Fruitful <fruitful2007@outlook.com>
2023-06-10 10:22:47 +0100

    idea float

--- resources/views/components/idea-float.blade.php
+++ resources/views/components/idea-float.blade.php
@@ -3,8 +3,8 @@
 
     <!-- Implement the FAB -->
 
-    <a href="/create/idea"  class="fixed z-90 bottom-10 right-2  w-20 h-20 rounded-full drop-shadow-lg flex justify-center bg-white shadow-md items-center text-white text-4xl hover:bg-gray-300 hover:drop-shadow-2xl hover:animate-bounce duration-300">
-        <svg style="width:15px; height:20px;" class="mt-1 ml-1 mr-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!-- Font Awesome Pro 5.15.4 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) --><path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/></svg></a>
+    <a href="/create/idea"  class="fixed z-90 bottom-10 right-2  w-20 h-20 rounded-full drop-shadow-lg flex justify-center bg-gradient-to-r from-cyan-500 to-blue-700 shadow-md items-center text-white text-5xl hover:bg-gray-300 hover:drop-shadow-2xl hover:animate-bounce duration-300">
+       +</a>
 
     <!-- Javascript code -->
    
