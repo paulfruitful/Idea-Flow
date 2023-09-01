988edbb90f58b13db244dbcb517a4da7ef2daac2
Paul Fruitful <fruitful2007@outlook.com>
2023-09-01 20:01:53 +0100

    Solution Tags column

---
+++ database/migrations/2023_09_01_185027_solution-tags.php
@@ -0,0 +1,33 @@
+<?php
+
+use Doctrine\DBAL\Schema\Schema as SchemaSchema;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('solutions', function(Blueprint $table){
+            $table->longText('tags');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
